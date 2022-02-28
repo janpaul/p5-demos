747294dfe906b2e3ce5b4fe0fa44e13be6d77af9
janpaul <janpaul@hey.com>
2022-02-28 14:26:31 +0100

    return early when the number is clearly not a prime

--- src/prime-number-spiral/script.ts
+++ src/prime-number-spiral/script.ts
@@ -1,9 +1,10 @@
 import p5 from "p5";
 
 const isPrime = (num: number): boolean => {
+  if (num <= 1) return false; // negatives
   for(let i = 2, s = Math.sqrt(num); i <= s; i++)
     if(num % i === 0) return false;
-  return num > 1;
+  return true;
 };
 
 new p5(p => {
